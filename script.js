7fbb4d30ef80e5e80998f6ce904c0537dbf831b8
mAttar11 <mattarannaufal@gmail.com>
2022-06-04 20:47:09 +0700

    menghapus fitur text change

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 console.log("hello world");
 
-// fitur text change
-let count = 0;
-const test = ["First Repo", "Repo Test", "Repo Beginner", "Hello World"];
-const header = setInterval(() => {
-	document.querySelector("h1").innerText = test[count];
-	count++;
-	if (count == test.length) {
-		count = 0;
-	}
-}, 1500);
+const header = document.querySelector("h1");
+header.innerText = "My first repo";
