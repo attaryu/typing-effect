2126fa5fe32c16d17b76b20bc0791720251f7333
mAttar11 <mattarannaufal@gmail.com>
2022-06-04 20:37:59 +0700

    menambahkan fitur text change pada javascript atau script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 console.log("hello world");
 
-const header = document.querySelector("h1");
-header.innerText = "My first repo";
+// fitur text change
+let count = 0;
+const test = ["First Repo", "Repo Test", "Repo Beginner", "Hello World"];
+const header = setInterval(() => {
+	document.querySelector("h1").innerText = test[count];
+	count++;
+	if (count == test.length) {
+		count = 0;
+	}
+}, 1500);
