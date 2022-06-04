39b15ff88344875acc063cefd3de93a243187b99
mAttar11 <mattarannaufal@gmail.com>
2022-06-05 03:52:20 +0700

    menambahkan eslint pada project dan refactory javascript part 2

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("hello world");
 // fitur text change
 let count = 0;
 const test = ["First Repo", "Repo Test", "Repo Beginner", "Hello World"];
-const header = setInterval(() => {
+setInterval(() => {
 	document.querySelector("h1").innerText = test[count++];
 	if (count == test.length) count = 0;
 }, 1500);
