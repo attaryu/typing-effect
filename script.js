59338b3205830b2a3d53c4cb76ca700c92908187
mAttar11 <mattarannaufal@gmail.com>
2022-06-05 03:45:26 +0700

    refactory script.js part 1

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ console.log("hello world");
 let count = 0;
 const test = ["First Repo", "Repo Test", "Repo Beginner", "Hello World"];
 const header = setInterval(() => {
-	document.querySelector("h1").innerText = test[count];
-	count++;
-	if (count == test.length) {
-		count = 0;
-	}
+	document.querySelector("h1").innerText = test[count++];
+	if (count == test.length) count = 0;
 }, 1500);
