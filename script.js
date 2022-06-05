3ae3ef46893408869b7e0153cf4ae809c1a0e2e4
mAttar11 <mattarannaufal@gmail.com>
2022-06-05 07:10:07 +0700

    typing animation pt.1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // fitur text change
+const header = document.querySelector(".title");
+const data = header.getAttribute("data-typing");
+let textNow = header.innerText.length;
+
+const eraser = setInterval(() => {
+	header.innerText += "";
+	textNow--;
+	console.log(textNow);
+	if (textNow === 0) {
+		clearInterval(eraser);
+	}
+}, 200);
