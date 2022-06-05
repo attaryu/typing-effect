c84449dedac3492d8f426a588c5c7d9a38bbb722
mAttar11 <mattarannaufal@gmail.com>
2022-06-05 08:09:40 +0700

    typing animation pt.3

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 // fitur text change
-const header = document.querySelector(".title");
+const header = document.querySelector(".title .title-text");
 const pradata = [
 	"Hello, I am Attar.",
 	"I'm a Full Stack developer.",
-	"I'm also a graphic designer.",
+	"I'm a graphic designer.",
 	"Nice to meet you.",
 ];
 const data = pradata.map((item) => textToArray(item));
 
-const eraser = setInterval(() => {}, 200);
+for (let i = 0; i < data.length; i++) {
+	for (let j = 0; j <= data[i].length; j++) {
+		header.innerHTML += `${data[i][j]}`;
+	}
+	header.innerHTML = "";
+}
+
+const interval = setInterval(() => {}, 200);
 
 function textToArray(text) {
 	const array = [];
-	for (const arr of text) {
-		array.push(arr);
-	}
-
+	for (const arr of text) array.push(arr);
 	return array;
 }
